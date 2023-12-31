d2c66dea2a478dff5ade082027d36714339f04d2
Talha <mdtalha2008@gmail.com>
2024-01-01 00:39:34 +0600

    header add

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
 
 const Main = () => {
     return (
         <div>
+            <Header></Header>
             <Outlet></Outlet>
         </div>
     );
