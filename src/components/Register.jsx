a8fa284cd9d8f35a9dfcbf87b52de9d76d1e0aea
Talha <mdtalha2008@gmail.com>
2023-12-31 22:47:08 +0600

    register add

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 
 const Register = () => {
     return (
         <div>
-            
+            <h1>This is register</h1>
         </div>
     );
 };
