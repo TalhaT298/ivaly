c8eb38757de5a5ccb4de4cb4c631912aedbd9def
Talha <mdtalha2008@gmail.com>
2023-12-31 20:16:51 +0600

    firbase add

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5Hui_Yv8VkC2oWfN1WRznLi6DlMtl8GM",
+  authDomain: "ivaly-2a904.firebaseapp.com",
+  projectId: "ivaly-2a904",
+  storageBucket: "ivaly-2a904.appspot.com",
+  messagingSenderId: "349669423357",
+  appId: "1:349669423357:web:ac2450429bf97f6e5fa694"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
