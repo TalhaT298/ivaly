de18375f7b788cc99ec03ab1f5fd6877b67872c3
Talha <mdtalha2008@gmail.com>
2023-12-31 22:35:55 +0600

    add outlet in main.jsx

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const Main = () => {
     return (
         <div>
-            
+            <Outlet></Outlet>
         </div>
     );
 };
