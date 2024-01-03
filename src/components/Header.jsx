73f480193f8cb752c7023c3762681666c73dbc5f
Talha <mdtalha2008@gmail.com>
2024-01-03 21:43:36 +0600

    max add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import {Link } from 'react-router-dom'
 const Header = () => {
   return (
-    <div>
-      <div className="navbar bg-base-100">
+    <div >
+      <div className="navbar bg-base-100 max-w-[1400px] mx-auto">
         <div className="flex-1">
           <a className="btn btn-ghost text-xl">Ivaly</a>
         </div>
@@ -17,7 +17,7 @@ const Header = () => {
             />
           </div>
           <div className="dropdown dropdown-end">
-            <div
+            {/* <div
               tabIndex={0}
               role="button"
               className="btn btn-ghost btn-circle avatar"
@@ -28,7 +28,7 @@ const Header = () => {
                   src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
                 />
               </div>
-            </div>
+            </div> */}
             <ul
               tabIndex={0}
               className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
@@ -49,7 +49,7 @@ const Header = () => {
           </div>
         </div>
         <Link to="/">Home</Link>
-        <Link to="//login">Register</Link>
+        <Link to="/login">Register</Link>
       </div>
     </div>
   );
