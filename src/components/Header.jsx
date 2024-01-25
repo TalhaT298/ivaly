0460ac485cc6fe68c017dd052d1f3bf1e9dcc53c
Talha <mdtalha2008@gmail.com>
2024-01-25 21:21:38 +0600

    login add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -51,7 +51,8 @@ const Header = () => {
           </div>
         </div>
         <Link to="/">Home</Link>
-        <Link to="/login">Register</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/login">Login</Link>
       </div>
     </div>
   );
