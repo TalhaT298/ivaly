4abbf76a8614b3e37a3833cf76fa8acb59ac6549
Talha <mdtalha2008@gmail.com>
2024-01-26 19:10:26 +0600

    edit form

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -29,7 +29,7 @@ const Register = () => {
           <form onSubmit={handleRegister} className="card-body">
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Name</span>
+                <span className="label-text">Name.</span>
               </label>
               <input
                 type="text"
