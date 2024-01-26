6f098bbef5a92fddf10735aab21e87daddfee6cb
Talha <mdtalha2008@gmail.com>
2024-01-26 20:23:35 +0600

    user add in auth

--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,9 +1,10 @@
 import React, { createContext } from 'react';
 const AuthContext= createContext(null);
+const user={displayName:"Talha Tarique"}
 const AuthProviders = () => {
     return (
-        <AuthContext.Provider>
-            
+        <AuthContext.Provider value={user}>
+
         </AuthContext.Provider>
     );
 };
